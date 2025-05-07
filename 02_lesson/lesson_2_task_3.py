289011e7eb10bf10f8d8e6f9d0e33ac09db94f5a
s-constantin <94Salvador56@gmail.com>
2025-05-07 23:14:13 +0400

    lesson_1

---
+++ 02_lesson/lesson_2_task_3.py
@@ -0,0 +1,3 @@
+square = int(input("Ведите длину стороны квадрата: "))
+result = square * square
+print("Площадь квадрата:", result)
---
+++ 02_lesson/lesson_2_task_4.py
@@ -0,0 +1,10 @@
+fizz_buzz = int(input("Ведите число: "))
+for i in range(1, fizz_buzz + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
